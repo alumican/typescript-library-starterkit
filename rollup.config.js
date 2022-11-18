d61f459c58ae1989e1cd2a9eb458a5b8d34dfee4
alumican <alumican.net@gmail.com>
2022-11-18 13:32:27 +0900

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import PluginCleanup from 'rollup-plugin-cleanup';
+import PluginNodeResolve from '@rollup/plugin-node-resolve';
+import PluginTypescript from "@rollup/plugin-typescript";
+import {terser as PluginTerser} from 'rollup-plugin-terser';
+
+import pkg from "./package.json";
+
+const production = process.env.BUILD === 'production';
+const document = process.env.BUILD === 'document';
+
+function getPlugins() {
+	const plugins = [
+		PluginTypescript({ tsconfig: './tsconfig.' + (document ? 'doc' : 'main') + '.json' }),
+		PluginNodeResolve({ preferBuiltins: false }),
+		PluginCleanup({ comments: 'none' }),
+	];
+	if (production) {
+		plugins.push(PluginTerser());
+	}
+	return plugins;
+}
+
+function getOutput() {
+	const output = {
+		file: production ? pkg.main.replace('.js', '.min.js') : pkg.main,
+		format: 'umd',
+		name: pkg.config.namespace,
+	};
+	if (!document) {
+		output['banner'] = `/*! ${pkg.name} ${pkg.version} (c) ${(new Date()).getFullYear()} ${pkg.author}, licensed under the ${pkg.license}, more information ${pkg.repository.url} */`;
+	}
+	return output;
+}
+
+export default {
+	input: './src/index.ts',
+	output: getOutput(),
+	plugins: getPlugins(),
+}
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export * from './typedef/Hash';
+
+export * from './core/Type';
+export * from './core/ArrUtil';
+export * from './core/DateUtil';
+export * from './core/NumUtil';
+export * from './core/ObjUtil';
+export * from './core/StrUtil';
+
+export * from './math/Easing';
